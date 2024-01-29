08ca94b80e455ee42475bfe1b74ff7525f61ea59
Thamizh <tamilselvancinnaiyan@gmail.com>
2024-01-30 00:11:22 +0530

    sample get api

--- controller/book.js
+++ controller/book.js
@@ -1,10 +1,15 @@
+import axios from 'axios';
+
 export const book = async (req, res) => {
     try {
-        
-        res.status(200).json({ message: 'Books working successfully' });
+        const genderizeResponse = await axios.get('https://api.genderize.io', {
+            params: {
+                name: 'Mary'
+            }
+        });
+        res.status(200).json(genderizeResponse.data);
     } catch (error) {
         console.error("Error occurred:", error.message);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
-  
